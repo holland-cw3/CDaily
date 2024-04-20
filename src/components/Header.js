b89cf4f3566f4ec983466eb9384b92e4da745d4a
Caleb Holland <101285025+holland-cw3@users.noreply.github.com>
2024-04-20 06:14:23 +0000

    stuff

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,7 +14,7 @@ function Header() {
   return (
     <header className="bg-black h-26">
       <div className="flex justify-between items-center h-full">
-        <a href="/"><img src={logo} alt="logo" className="logo mt-1 mb-1 ml-3" /></a>
+        <a href="/"><img src={logo} alt="logo" className="logo mt-1 mb-1 ml-5" /></a>
         <div className="flex mb-5 mr-3">
           <a href="/extension" className="text-white tab mt-4">Extension</a>
           <a href="/emails" className="text-white tab mt-4">Email</a>
--- src/pages/home.js
+++ src/pages/home.js
@@ -12,6 +12,8 @@ function home() {
  
   return (
       <div className="home flex justify-center">
+        <div className="flex"> </div>
+      
       <h2 className="absolute text-6xl font-bold text-[#7b9a6d] z-20 top-20"> Invest 
         <TypeAnimation className="text-white" sequence={[
           ' Small',2000,
@@ -24,6 +26,76 @@ function home() {
       <button className="homeBtn mr-10">Learn </button>
       <a href="/ratesandcalc"><button className="homeBtn">Start</button></a>
       </div>
+      
+
+      <section className=" z-22 flex flex-col items-center md:flex-row justify-center textContainer w-full">
+        <div className="mb-10 w-full bg-white bg-opacity-50 flex flex-col md:flex-row mr-(-5)">
+          <div className="align-left md:w-1/2">
+          <iframe width="560" className="mt-10 mb-10 ml-10 rounded-md" height="315" src="https://www.youtube.com/embed/_3cgMofw-rc?si=nj3VWG0hjsQHluDR" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          </div>
+          <div className="flex flex-col md:w-1/2 mr-20 mt-10">
+            <h1 className="text-black font-bold text-4xl mb-10 ">
+              What Are CDs?
+            </h1>
+            <p className="text-black mb-6 mt-5">
+              Please sign up to join us Mon-Fri at the{" "}
+              <a
+                href="https://www.google.com/maps/place/PP1,+College+Park,+MD+20740/@38.9933786,-76.9425631,148m/data=!3m2!1e3!4b1!4m2!3m1!1s0x89b7c69f4dc17645:0xd2d09f5ca7e6b0d9"
+                className="text-blue-700 font-bold"
+              >
+                School of Public Health (SPH)
+              </a>{" "}
+              East Side (closest to Xfinity Center) starting{" "}
+              <b>September 5th. Practices now start at 5pm! </b>
+            </p>
+            <p className="text-black mb-7">
+              Our mission is to support and encourage a healthy approach to
+              running and training for all members of the University of
+              Maryland. We provide an atmosphere in which runners of all
+              abilities can meet each other, share running knowledge and simply
+              enjoy running together as well as compete against other club
+              college teams.
+            </p>
+            
+          </div>
+        </div>
+      </section>
+
+      <section className=" z-22 flex flex-col items-center md:flex-row justify-center textContainer2 w-full">
+        <div className="mb-10 w-full bg-white bg-opacity-50 flex flex-col md:flex-row mr-(-5)">
+          <div className="align-left md:w-1/2">
+          <iframe width="560" className="mt-10 mb-10 ml-10 rounded-md" height="315" src="https://www.youtube.com/embed/_3cgMofw-rc?si=nj3VWG0hjsQHluDR" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          </div>
+          <div className="flex flex-col md:w-1/2 mr-20 mt-10">
+            <h1 className="text-black font-bold text-4xl mb-10 ">
+              What Is CDaily?
+            </h1>
+            <p className="text-black mb-6 mt-5">
+              Please sign up to join us Mon-Fri at the{" "}
+              <a
+                href="https://www.google.com/maps/place/PP1,+College+Park,+MD+20740/@38.9933786,-76.9425631,148m/data=!3m2!1e3!4b1!4m2!3m1!1s0x89b7c69f4dc17645:0xd2d09f5ca7e6b0d9"
+                className="text-blue-700 font-bold"
+              >
+                School of Public Health (SPH)
+              </a>{" "}
+              East Side (closest to Xfinity Center) starting{" "}
+              <b>September 5th. Practices now start at 5pm! </b>
+            </p>
+            <p className="text-black mb-7">
+              Our mission is to support and encourage a healthy approach to
+              running and training for all members of the University of
+              Maryland. We provide an atmosphere in which runners of all
+              abilities can meet each other, share running knowledge and simply
+              enjoy running together as well as compete against other club
+              college teams.
+            </p>
+            
+          </div>
+        </div>
+      </section>
+
+      
+
       
 
 
